871a672f5a7fd910524d0d4d3565e3e97cf4a678
EduardoGutierrezDizeo <edojose1518@gmail.com>
2026-05-27 21:55:31 -0500

    se agrego boolean en el cast de product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,6 +26,7 @@ class Product extends Model
         'stock' => 'integer',
         'minimun_stock' => 'integer',
         'selling_price' => 'float',
+        'status'        => 'boolean',
     ];
 
     public function productType()
